750f352f0f2a6a2adef5424d8d9e6dd3b5a6e158
Jeremy Bost <bostjs1@gcc.edu>
2016-12-10 19:49:08 -0500

    Photon cannon works. Needs particles

--- photonCannon.cpp
+++ photonCannon.cpp
@@ -41,15 +41,16 @@ void PhotonCannon::update(float frameTime)
 {
 	Entity::update(frameTime);
 	if(cooldown <= photonCannonNS::TIME_BETWEEN_SHOTS) cooldown += frameTime;
-	gunImage.setRadians(gunImage.getRadians() + frameTime * photonCannonNS::ROTATE_SPEED);
+	if (target == nullptr) {
+		gunImage.rotate(frameTime, photonCannonNS::ROTATE_SPEED);
+	}
 	gunImage.update(frameTime);
-
-	projectile.update(frameTime);
 
 	if (projectile.getActive())
 	{
-		
+		projectile.moveTo(frameTime, photonCannonNS::PROJECTILE_SPEED, targetX, targetY);
 	}
+	projectile.update(frameTime);
 }
 
 void PhotonCannon::drawProjectiles()
@@ -65,31 +66,82 @@ void PhotonCannon::attackTarget(Entity * target)
 void PhotonCannon::attackTargets(std::list<Enemy*> targets)
 {
 	this->targets = targets;
-	if (cooldown >= photonCannonNS::TIME_BETWEEN_SHOTS) {
-		float dist = 2000.0f;
-		Enemy* closest = nullptr;
-		for (auto enemy = targets.begin(); enemy != targets.end(); enemy++) {
-			float newDist = std::sqrt(std::pow((*enemy)->getCenterX() - getCenterX(), 2) + std::pow((*enemy)->getCenterY() - getCenterY(), 2));
-			if (getRange() >= newDist) {
-				if (newDist < dist) {
-					closest = *enemy;
-					dist = newDist;
-				}
+	float dist = 9999999.0f; // using dist squared
+	Enemy* closest = nullptr;
+	for (auto enemy = targets.begin(); enemy != targets.end(); enemy++) {
+		float newDist = std::pow((*enemy)->getCenterX() - getCenterX(), 2) + std::pow((*enemy)->getCenterY() - getCenterY(), 2);
+		if (std::pow(getRange(), 2) >= newDist) {
+			if (newDist < dist) {
+				closest = *enemy;
+				dist = newDist;
 			}
 		}
-		target = closest;
+	}
+	
+	target = closest;
 
-		if (target != nullptr) {
-			projectile.setX(getCenterX() - projectile.getWidth() * projectile.getScale() / 2);
-			projectile.setY(getCenterY() - projectile.getHeight() * projectile.getScale() / 2);
-
-			projectile.setActive(true);
-			projectile.setVisible(true);
-		}
-		
+	// fire new photon if cooldown is up and there is no projectile out
+	if (closest != nullptr && cooldown >= photonCannonNS::TIME_BETWEEN_SHOTS 
+		&& !projectile.getActive())
+	{
 		cooldown = 0;
+		projectile.setX(getCenterX() - projectile.getWidth() * projectile.getScale() / 2);
+		projectile.setY(getCenterY() - projectile.getHeight() * projectile.getScale() / 2);
+
+		projectile.setActive(true);
+		projectile.setVisible(true);
+		gunImage.setCurrentFrame(photonCannonNS::PROJECTILE_FRAME_START);
 		audio->playCue(LASER);
 	}
+
+	
+
+	// update target coords if target exists
+	if (target != nullptr)
+	{
+		targetX = target->getCenterX();
+		targetY = target->getCenterY();
+
+		gunImage.setRadians(gunImage.getRadians() + gunImage.angleToTarget(targetX, targetY) + PI/2);
+	}
+	else
+	{
+		projectile.setActive(false);
+		projectile.setVisible(false);
+	}
+
+	// check for projectile collisions with enemies and explode if one happens
+	if (projectile.getActive())
+	{
+		bool hit = false;
+
+		for (auto enemy = targets.begin(); enemy != targets.end(); enemy++) {
+			if ((*enemy)->collidesWith(projectile, VECTOR2()))
+			{
+				hit = true;
+				break;
+			}
+		}
+
+		// check if should explode
+		if (hit || (projectile.getCenterX() == targetX && projectile.getCenterY() == targetY))
+		{
+			// increase collision radius to explosion range and apply damage to all collisions
+			projectile.setCollisionRadius(photonCannonNS::PROJECTILE_EXPLOSION_RADIUS);
+			for (auto enemy = targets.begin(); enemy != targets.end(); enemy++) {
+				if ((*enemy)->collidesWith(projectile, VECTOR2()))
+				{
+					(*enemy)->setHealth((*enemy)->getHealth() - photonCannonNS::DAMAGE);
+				}
+			}
+			// set back
+			projectile.setCollisionRadius(projectile.getWidth() * projectile.getScale() / 2);
+			projectile.setActive(false);
+			projectile.setVisible(false);
+			this->target = nullptr;
+		}
+	}
+
 }
 
 void PhotonCannon::setProjectileTexture(TextureManager * tm)
@@ -108,11 +160,12 @@ void PhotonCannon::setProjectileTexture(TextureManager * tm)
 	projectile.setColorFilter(graphicsNS::WHITE);
 	projectile.setActive(false);
 	projectile.setVisible(false);
+	projectile.setCollisionRadius(projectile.getWidth() * projectile.getScale() / 2);
 }
 
 void PhotonCannon::setGunTexture(TextureManager * tm)
 {
-	if (!gunImage.initialize(graphics, 126, 345, 8, tm)) {
+	if (!gunImage.initialize(gamePtr, 126, 345, 8, tm)) {
 		throw GameError(gameErrorNS::FATAL_ERROR, "Error initializing photon cannon gun image");
 	}
 	gunImage.setScale(photonCannonNS::GUN_IMAGE_SCALE);
@@ -121,5 +174,5 @@ void PhotonCannon::setGunTexture(TextureManager * tm)
 	gunImage.setFrames(photonCannonNS::GUN_FRAME_START, photonCannonNS::GUN_FRAME_END);
 	gunImage.setFrameDelay(photonCannonNS::GUN_FRAME_DELAY);
 	gunImage.setCurrentFrame(photonCannonNS::GUN_FRAME_END);
-	gunImage.setLoop(true);
+	gunImage.setLoop(false);
 }
--- photonCannon.h
+++ photonCannon.h
@@ -6,15 +6,15 @@
 #include "graphics.h"
 
 namespace photonCannonNS {
-	const int PRICE = 500;
+	const int	PRICE = 600;
 	const float RANGE = 300.0f;
-	const float TIME_BETWEEN_SHOTS = 1;//0.3f; // in seconds
+	const float TIME_BETWEEN_SHOTS = 2;//0.3f; // in seconds
 	const float ROTATE_SPEED = 0.5f; // radians per second
-	const int STARTING_HEALTH = 600;
-	const int DAMAGE = 50;
+	const int	STARTING_HEALTH = 600;
+	const int	DAMAGE = 100;
 
 	const float GUN_IMAGE_SCALE = 0.24f;
-	const float GUN_FRAME_DELAY = 0.08f;
+	const float GUN_FRAME_DELAY = 0.12f;
 	const int	GUN_FRAME_START = 0;
 	const int	GUN_FRAME_END = 7;
 	
@@ -22,7 +22,8 @@ namespace photonCannonNS {
 	const float PROJECTILE_FRAME_DELAY = 0.08f;
 	const int	PROJECTILE_FRAME_START = 0;
 	const int	PROJECTILE_FRAME_END = 3;
-	const float PROJECTILE_SPEED = 20.0f;
+	const float PROJECTILE_SPEED = 350.0f;
+	const float	PROJECTILE_EXPLOSION_RADIUS = 400.0f;
 }
 
 class PhotonCannon : public Structure
@@ -42,6 +43,7 @@ public:
 	// Not supported. Don't use.
 	void attackTarget(Entity* target);
 
+	// call each frame. Pass all enemies and it will pick one and target it.
 	void attackTargets(std::list<Enemy*> targets);
 
 	float getRange() { return photonCannonNS::RANGE; }
@@ -53,9 +55,11 @@ public:
 
 private:
 	Entity projectile;
-	Image gunImage;
+	Entity gunImage;
 	Enemy* target;
 	std::list<Enemy*> targets;
 	float cooldown;
+
+	int targetX, targetY;
 };
 
